19a153b676dfb7a25d1a48c76521f629223bf46a
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-15 13:55:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string = "Squawk!")
-  puts #{string}
-  return string
+def parrot(arg = "Squawk!")
+  puts #{arg}
+  return arg
 end
 parrot
